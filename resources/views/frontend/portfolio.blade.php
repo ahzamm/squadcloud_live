1f27b3ed7c2a4b382d66313644675b7d0fd15728
Ahzam Ahmed <ahzamahmed6@gmail.com>
2024-06-07 19:03:30 +0500

    add modal file from live in client page

--- resources/views/frontend/portfolio.blade.php
+++ resources/views/frontend/portfolio.blade.php
@@ -28,13 +28,19 @@
    .product-list button {
       display: inline-block;
       width: auto;
-      padding: 0.6rem 1.5rem;
+      padding: 10px 0;
       border: 1px solid #961b04;
       background: none;
       font-weight: 400;
       border-radius: 50px;
       cursor: pointer;
       color: #961b04;
+      flex: 1 1 auto;
+   }
+   .product-list a:hover,
+   .product-list button:hover {
+      background: #961b04;
+      color: #fff !important;
    }
    .product_container {
       margin: 0 auto 0 auto;
@@ -51,26 +57,27 @@
       opacity: 0.7;
    }
    .product {
-      height: 300px;
-      padding-bottom: 20px;
+      height: 230px;
+      /* padding-bottom: 20px; */
       border-radius: 10px;
       background-color: #fff;
       display: flex;
       flex-direction: column;
-      justify-content: space-between;
+      /* justify-content: space-between; */
       align-items: center;
       transition: filter 0.1s ease-in-out, opacity 0.1s ease-in-out;
    }
    .product-list .img {
       height: 60%;
       width: 100%;
+      flex: 1 0 auto;
    }
    .product-list .info {
       display: flex;
       justify-content: space-between;
       align-items: center;
       width: 100%;
-      padding: 0 15px;
+      padding: 12px 15px;
    }
 </style>
 
@@ -85,16 +92,21 @@
       <div class="product_container">
          <div class="product-list">
             @forelse ($portfolios as $portfolio)
+            <div>
                <div class="product" data-price="50">
                   <div class="img">
                      <img src="frontend_assets/images/portfolio/{{$portfolio->image}}" alt="{{ $portfolio->title }}">
                   </div>
                   <div class="info">
                      <h5>{{ $portfolio->title }}</h5>
-                     <p><i class="fa fa-heart"></i></p>
+                     <!-- <p><i class="fa fa-heart"></i></p> -->
                   </div>
-                  <button><a href="{{$portfolio->link}}">View Demo</a></button>
                </div>
+               <div class="product-btns d-flex mt-2" style="column-gap:5px">
+                  <button><a href="{{$portfolio->link}}">Request Demo</a></button>
+                  <button><a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#portfolio-detail-modal">More Info</a></button>
+               </div>
+            </div>
             @empty
                <div class="alert alert-danger">No Record Found!</div>
             @endforelse
@@ -102,5 +114,23 @@
       </div>
    </div>
 </section>
-
+<div class="modal" tabindex="-1" role="dialog" id="portfolio-detail-modal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
